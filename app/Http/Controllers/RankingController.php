71b22181465639747a9dc66719e9bb7a5fa2ade8
EC2 Default User <ec2-user@ip-172-31-33-50.us-east-2.compute.internal>
2018-06-28 05:21:48 +0000

    have ranking

--- app/Http/Controllers/RankingController.php
+++ app/Http/Controllers/RankingController.php
@@ -85,9 +85,21 @@ class RankingController extends Controller
      public function want()
     {
         $items = \DB::table('item_user')->join('items', 'item_user.item_id', '=', 'items.id')->select('items.*', \DB::raw('COUNT(*) as count'))->where('type', 'want')->groupBy('items.id', 'items.code', 'items.name', 'items.url', 'items.image_url','items.created_at', 'items.updated_at')->orderBy('count', 'DESC')->take(10)->get();
-
+        $type = 'want';
         return view('ranking.want', [
+            'type' => $type,
+            'items'=> $items,
+        ]);
+    }
+    
+    public function have()
+    {
+        $items = \DB::table('item_user')->join('items', 'item_user.item_id', '=', 'items.id')->select('items.*', \DB::raw('COUNT(*) as count'))->where('type', 'have')->groupBy('items.id', 'items.code', 'items.name', 'items.url', 'items.image_url','items.created_at', 'items.updated_at')->orderBy('count', 'DESC')->take(10)->get();
+        $type = 'have';
+        return view('ranking.have', [
+            'type' => $type,
             'items' => $items,
+            
         ]);
     }
 }
